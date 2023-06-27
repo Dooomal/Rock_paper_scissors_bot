dd224e2ae8c252b165f5c59a8016da1542277bf3
Alexey <komkov08@gmail.com>
2023-06-27 11:44:42 +0300

    Добавлен хендлер обрабатывающий команду старт.

---
+++ telegram_bot_template/handlers/user_handlers.py
@@ -0,0 +1,13 @@
+from aiogram import Router
+from aiogram.filters import CommandStart, Command, Text
+from aiogram.types import Message
+from keyboards.keyboards import game_kb, yes_no_kb
+from lexicon.lexicon_ru import LEXICON_RU
+from services.services import get_bot_choice, get_winner
+
+router: Router = Router()
+
+@router.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(text=LEXICON_RU['/start'], reply_markup=yes_no_kb)
+
